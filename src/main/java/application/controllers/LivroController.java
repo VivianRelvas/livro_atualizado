45c222b1d6afa889f7917827707004f8a8d30401
VivianRelvas <vivianrelvas@hotmail.com>
2021-10-26 19:55:50 +0000

    Atualizando o arquivo Delete

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,6 +1,9 @@
 package application.controllers;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,5 +38,15 @@ public class LivroController {
     livrosRepo.save(livro); //objeto é livro minúsculo e fosse classe era Livro maiúsculo
     
     return "redirect:/livro/list";
+  }
+
+  @RequestMapping("/delete/{id}")
+  public String formDelete(Model model,@PathVariable int id){     //@ anotação   model é um objeto
+   Optional<Livro>livro=livrosRepo.findById(id);    // classe <Livro>  e objeto do tipo livro recebe todos os livros que tem e seus ids
+    
+        if(!livro.isPresent())  // ! inverte o que é V vira F e vice-versa
+          return "redirect:/livro/list";
+        model.addAttribute("livro",livro.get());
+          return "/livro/delete.jsp";
   }
 }
